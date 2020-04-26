d54dcd8fc202bba1ee347b9fcc0fde40eec01cad
Tomasz Krutowski <tkrutowski@gmail.com>
2020-04-26 09:48:41 +0200

    wrong city error

--- src/main/java/pl/sda/weather/GetWeather.java
+++ src/main/java/pl/sda/weather/GetWeather.java
@@ -1,6 +1,7 @@
 package pl.sda.weather;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -9,16 +10,22 @@ import java.util.Scanner;
 
 public class GetWeather {
     public static void main(String[] args) throws IOException {
-       final String  KLUCZ_API="66bbbddf38facc47e28cb6afc7bccc6b";
+        final String KLUCZ_API = "66bbbddf38facc47e28cb6afc7bccc6b";
         ObjectMapper mapper = new ObjectMapper();
-        WeatherSerive weatherSerive = new WeatherSerive("http://api.weatherstack.com/current",KLUCZ_API);
+        WeatherSerive weatherSerive = new WeatherSerive("http://api.weatherstack.com/current", KLUCZ_API);
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Podaj miasto:");
         String city = scanner.nextLine();
-        Weather weather = weatherSerive.getCityWeather(city);
 
-        System.out.println("Temperatura w " + weather.getLocation().getName() + " wynosi: " +weather.getCurrent().getTemperature());
+        try {
+            Weather weather = weatherSerive.getCityWeather(city);
+
+            System.out.println("Temperatura w " + weather.getLocation().getName() + " wynosi: " + weather.getCurrent().getTemperature());
+        } catch (UnrecognizedPropertyException e) {
+            System.out.println("Podane miasto nie istnieje.");
+        }
+
 
     }
 }
--- src/main/java/pl/sda/weather/WeatherSerive.java
+++ src/main/java/pl/sda/weather/WeatherSerive.java
@@ -1,21 +1,22 @@
 package pl.sda.weather;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class WeatherSerive {
-
     private String finalURL;
     private Weather weather;
     ObjectMapper mapper = new ObjectMapper();
+
     public WeatherSerive(String url, String key) {
-        finalURL=url+ "?access_key=" +key;
+        finalURL = url + "?access_key=" + key;
     }
+
     public Weather getCityWeather(String city) throws IOException {
-        finalURL = finalURL+ "&query=" + city;
-        return mapper.readValue(new URL(finalURL),Weather.class);
+        finalURL = finalURL + "&query=" + city.replace(" ", "%20");
+        return mapper.readValue(new URL(finalURL), Weather.class);
     }
 }
